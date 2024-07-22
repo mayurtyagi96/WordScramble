307c4524c954761a302a7905c31475a5e117f734
Mayur <mayurtyagi@unolo.com>
2024-07-22 19:33:26 +0530

    test

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -11,7 +11,6 @@ struct ContentView: View {
     @State private var usedWords = [String]()
     @State private var rootWord = ""
     @State private var newWord = ""
-    
     @State private var alertTitle = ""
     @State private var alertMsg = ""
     @State private var showAlert = false
